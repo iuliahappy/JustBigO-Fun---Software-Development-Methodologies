781c80aee117d80c143b8df0cd030aa505716ea7
Iulia Maria Popescu <iuliamaria0201@gmail.com>
2026-03-16 10:34:16 +0200

    Frontend - version 1 + .gitignore

---
+++ JustBigO(Fun)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JustBigO_Fun_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
